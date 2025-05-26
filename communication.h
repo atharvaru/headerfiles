09b3ae283ebb8aab11ee60202f909ce6a4cb937c
atharvaru <160043531+atharvaru@users.noreply.github.com>
2025-05-26 10:57:56 -0400

    Create communication.h

---
+++ communication.h
@@ -0,0 +1,16 @@
+#ifndef communications_h
+#define communications_h
+
+#include <string>
+#include <algorithm>
+
+
+std::string greeting(std::string& name);
+
+void formatAddress(std::string* address);
+
+int generateRandomTime();
+
+std::string closing( std::string& product);
+
+#endif 
